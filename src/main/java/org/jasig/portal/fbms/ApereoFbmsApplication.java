15fa8ba24f94b1ae27093829ec0213c2755855f3
Drew Wills <wills.drew@gmail.com>
2018-05-08 15:22:04 -0700

    Apereo FBMS project as originally generated by http://start.spring.io/

---
+++ src/main/java/org/jasig/portal/fbms/ApereoFbmsApplication.java
@@ -0,0 +1,12 @@
+package org.jasig.portal.fbms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApereoFbmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApereoFbmsApplication.class, args);
+	}
+}
