04e0ec22e36f3780247514a89897fecea377e098
Drew Wills <wills.drew@gmail.com>
2018-06-25 17:12:21 -0700

    chore(deps):  upgrade to Spring Boot 2.0.3 and Gradle 4.8.1

--- src/main/java/org/apereo/portal/fbms/ServletInitializer.java
+++ src/main/java/org/apereo/portal/fbms/ServletInitializer.java
@@ -1,7 +1,7 @@
 package org.apereo.portal.fbms;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
