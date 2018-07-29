875bf57730d465b704ecaf29b74ebc306812d82c
Drew Wills <wills.drew@gmail.com>
2018-07-29 15:16:57 -0700

    fix(Swagger):  restore access to the swagger-ui.html page

--- fbms-webapp/src/main/java/org/apereo/portal/fbms/WebConfiguration.java
+++ fbms-webapp/src/main/java/org/apereo/portal/fbms/WebConfiguration.java
@@ -15,6 +15,10 @@ public class WebConfiguration implements WebMvcConfigurer {
      */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
         registry
                 .addResourceHandler("/webjars/**")
                 .addResourceLocations("/webjars/");
